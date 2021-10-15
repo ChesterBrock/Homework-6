046f4eea53407a80eeea2b6c4dab1bdcf87f8a57
Chester Brock <cj.rozor@gmail.com>
2021-10-15 00:01:58 -0700

    Project2 final edits.

--- Project 2/Program.cs
+++ Project 2/Program.cs
@@ -10,7 +10,11 @@
 /// Overall Plan:
 /// 1) Create three meathods : create array, reverse array, print array
 /// 2) The GenerateNumbers method should return an array of 10 numbers.
-/// 3) 
+/// 3) The PrintNumber method should simply use a for or foreach loop to go down the array,
+/// 4) the reverse method - Use a for loop, starting at 0 and going up the length of the array / 2.
+/// 5) The number you use in the for loop will be the index of the first number to swap
+/// 6) swapping array[index] with array[arrayLength - index - 1].
+///
 ///
 
 using System;
@@ -26,13 +30,8 @@ namespace Project_2
         static void Main(string[] args)
         {
             int[] numbers = GenerateNumbers();
-            foreach(int element in numbers)
-            {
-                Console.Write(element + ", ");
-            }
-            Console.ReadLine();
-            //Reverse(numbers);
-            //Printnumber(numbers);
+            Printnumber(numbers);
+            Reverse(numbers);
         }
         // end of main method 
 
@@ -47,6 +46,38 @@ namespace Project_2
             }
             return tempArray;
         }
+
+        // The PrintNumber method should simply use a for or foreach loop to go down the array, one at a time,
+        // and print out the items in it.
+        public static void Printnumber(int[] numbers)
+        {
+            foreach(int element in numbers)
+            {
+                Console.Write(element + " ");
+            }
+        }
+
+        //The Reverse method
+        public static void Reverse(int[] array)
+        {
+            // hint- temporary location to make the swap:
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                int temp = array[i];
+                array[i] = array[array.Length - i - 1];
+                array[array.Length - i - 1] = temp;
+            }
+
+            Console.WriteLine("\nReversed array ~~ ");
+            foreach (int element in array)
+            {
+                Console.Write(element + " ");
+            }
+            Console.ReadLine();
+
+        }
+        
+        
 
     }
 }
