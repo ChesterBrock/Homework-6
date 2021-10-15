96f04355952f0d3a5dd2beab7175bf58db15c687
Chester Brock <cj.rozor@gmail.com>
2021-10-14 20:34:06 -0700

    Add project files.

---
+++ Project1/Program.cs
@@ -0,0 +1,48 @@
+﻿/// Chapter No. 6		Exercise No. 1
+/// File Name: Program.cs
+/// @author: Chester Brock 
+/// Date:  Oct 14, 2021
+///
+/// Problem Statement: Write code to create a copy of an array.
+/// 
+/// 
+/// 
+/// Overall Plan:
+/// 1)  First, start by creating an initial array. 
+/// 2)  start with 10 items.
+/// 3)  Declare an array variable and assign it a new
+/// 4)  create a second array variable.
+/// 5)  Give it a new array with the same length as the first.  
+/// 6)  use the Length property to get the size of the original array.
+/// 7)  Use a loop to read values from the original array and place them in the new array.
+/// 8)  print out the contents of both arrays, to be sure everything copied correctly.
+///
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create original array 
+            double[] score = new double[10];
+            
+            for (int i = 0; i < score.Length; i++)
+            {
+                // prompting to fill the 10 elements 
+                Console.Write("Array slot #" + i+1 + ":  ");
+                score[i] = double.Parse(Console.ReadLine());
+            }
+            // Alerting the array has been filled 
+            Console.WriteLine("The array has been filled ( Press Enter) ");
+
+            Console.ReadLine();
+        }
+    }
+}
